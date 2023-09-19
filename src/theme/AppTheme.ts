5c5d66025562eb7d0f8ac38bad17ebff3ccda8c9
Pasupathi-M <pasupathi.muthukumar@kprinfo.com>
2023-09-20 00:14:57 +0530

    Theme added

---
+++ src/theme/AppTheme.ts
@@ -0,0 +1,45 @@
+import { createTheme, colors } from "@mui/material";
+
+export const theme = createTheme({
+  breakpoints: {
+    keys: ["xs", "sm", "md", "lg", "xl"],
+    values: {
+      xs: 0,
+      sm: 600,
+      md: 900,
+      lg: 1200,
+      xl: 1536,
+    },
+  },
+  palette: {
+    mode: "light",
+    common: {
+      black: "#000",
+      white: "#fff",
+    },
+    primary: {
+      main: colors.amber[400],
+      light: colors.amber[200],
+      dark: colors.amber[600],
+    },
+    secondary: {
+      main: colors.amber["A400"],
+      dark: colors.amber["A700"],
+      light: colors.amber["A100"],
+    },
+    error: {
+      main: colors.red["500"],
+      dark: colors.red["800"],
+      light: colors.red["300"],
+    },
+  },
+  shape: {
+    borderRadius: 5,
+  },
+  zIndex: {
+    appBar: 1100,
+    modal: 1300,
+    snackbar: 1400,
+    tooltip: 1500,
+  },
+});
