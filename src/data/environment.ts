ae13521792e047c5f69ab251fd38578b93c5bf13
Pasupathi-M <pasupathi.muthukumar@kprinfo.com>
2023-10-25 22:54:48 +0530

    env added

--- src/data/environment.ts
+++ src/data/environment.ts
@@ -1,9 +1,9 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
 // require('dotenv').config()
 
-console.log('Environment')
-export const  BASE_API_ENDPOINT = 'http://localhost:2111'
-
+console.log('Environment', import.meta.env.DEV)
+export const  BASE_API_ENDPOINT = import.meta.env.DEV ? "http://localhost:2111" : "https://expo-api.onrender.com"
+console.log('BASE_API_ENDPOINT', BASE_API_ENDPOINT)
 
 export const API_ROUTES = Object.freeze({
     AUTH: {
